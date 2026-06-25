67a5743cef30982f2d14420c63f824c59618f917
eric_huang <godhand4826@gmail.com>
2026-06-25 23:42:07 +0800

    Updates

---
+++ precache-manifest.687101204df8fe3f0ace54a49746e1ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58becf8c3817d47151a7",
+    "url": "/game-of-life/static/css/main.48fc71a9.chunk.css"
+  },
+  {
+    "revision": "58becf8c3817d47151a7",
+    "url": "/game-of-life/static/js/main.58becf8c.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/game-of-life/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "48c5f91bb349b88c84a7",
+    "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
+  },
+  {
+    "revision": "2551fdc66abc4494cccaf747037a0fa5",
+    "url": "/game-of-life/index.html"
+  }
+];
